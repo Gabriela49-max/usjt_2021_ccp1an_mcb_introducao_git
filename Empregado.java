71e3c6422e80ebbe5507ea33a4e22428cc566213
Gabriela Duarte <gabi.art@hotmail.com>
2021-03-09 20:57:22 -0300

    Commit inicial.Classe Empregado criada

---
+++ Empregado.java
@@ -0,0 +1,20 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public void setNome(String Nome){
+        this.nome = nome;
+    }
+}
